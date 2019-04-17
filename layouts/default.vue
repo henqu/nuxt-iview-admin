8ec0b7ea07121a706175da17f5b7de4a55eb3255
henq <taibaizhou@163.com>
2019-04-17 23:59:28 +0800

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{name}}</h1>
+    <nuxt />
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      name: "henq222"
+    }
+  }
+}
+</script>
+<style lang="less">
+.size{
+  width: 100%;
+  height: 100%;
+}
+html,body{
+  .size;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  .size;
+}
+</style>
